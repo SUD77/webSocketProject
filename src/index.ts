b2f109e65df3dab97da25f826fb441b9e5e25d4e
SUD77 <jaiswalsudhanshu20@gmail.com>
2025-07-29 10:12:56 +0530

    feat: code update

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,15 @@ import { WebSocketServer } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
 
 //event handler
-// wss.on("connection", function connection(ws) {
-//   ws.on("error", console.error);
-
-//   ws.on("message", function message(data) {
-//     console.log("received: %s", data);
-//   });
-
-//   ws.send("something");
-// });
-
-//event handler
-wss.on("connection", function(socket){
+wss.on("connection", function (socket) {
   console.log("user connected");
-  setInterval(()=>{
-    socket.send("Meow Meow : "+ Math.random()*10);
-  },1000)
 
   //receive msg from client
-  socket.on("message", (e)=>{
-    console.log(e.toString());
-  })
-})
+  socket.on("message", (e) => {
+    if (e.toString() === "ping") {
+      socket.send("pong");
+    } else {
+      socket.send("Meow");
+    }
+  });
+});
